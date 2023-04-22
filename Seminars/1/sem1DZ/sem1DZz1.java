fa4fc32c9504ea89f4655ec57d784e8f906b2648
“Alexandr <kuzmin_89@mail.ru>
2023-04-22 19:55:32 +0500

    1semDZ

---
+++ Seminars/1/sem1DZ/sem1DZz1.java
@@ -0,0 +1,18 @@
+//Вычислить n-ое треугольного число (сумма чисел от 1 до n), n! (произведение чисел от 1 до n)
+import java.util.Scanner;
+public class sem1DZz1 {
+    public static void main(String[] args) {
+        Scanner iScanner = new Scanner(System.in);
+        System.out.print("Введите число N: ");
+        int N = iScanner.nextInt();
+        iScanner.close();
+        int sumNum = 0, sumFac = 1;
+        for (int i = 1; i < N + 1; i++) {
+            sumNum = sumNum + i;
+            sumFac = sumFac * i;
+        }
+        System.out.printf("Число N = %d", N);
+        System.out.printf("\nСумма чисел от 1 до N = %d", sumNum);
+        System.out.printf("\nФакториал числа N = %d", sumFac);
+    }
+}
